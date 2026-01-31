36d88296d2df7db5cc808edcd754d95c44bd1d60
Sidharth <sidharth2020sharma@gmail.com>
2026-01-31 23:29:28 +0530

    Add Validations and global exceptions

--- src/main/java/com/ecommerce/project/repositories/CategoryRepositroy.java
+++ src/main/java/com/ecommerce/project/repositories/CategoryRepositroy.java
@@ -4,4 +4,5 @@ import com.ecommerce.project.model.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CategoryRepositroy extends JpaRepository<Category,Long> {
+    Category findByCategoryName(String categoryName);
 }
--- src/main/java/com/ecommerce/project/service/CategoryServiceImpl.java
+++ src/main/java/com/ecommerce/project/service/CategoryServiceImpl.java
@@ -1,5 +1,7 @@
 package com.ecommerce.project.service;
 
+import com.ecommerce.project.exceptions.APIException;
+import com.ecommerce.project.exceptions.ResourceNotFoundException;
 import com.ecommerce.project.model.Category;
 import com.ecommerce.project.repositories.CategoryRepositroy;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,24 +20,31 @@ public class CategoryServiceImpl implements CategoryService {
 
     @Override
     public List<Category> getAllCategories() {
+        List<Category> categories = categoryRepositroy.findAll();
+        if (categories.isEmpty())
+            throw new APIException("No category created till now");
+
         return categoryRepositroy.findAll();
     }
 
     @Override
     public void createCategory(Category category) {
+        Category savedCategory = categoryRepositroy.findByCategoryName(category.getCategoryName());
+        if (savedCategory != null)
+            throw new APIException("Category with the name "+category.getCategoryName() + " already exists!!!");
         categoryRepositroy.save(category);
     }
 
     @Override
     public String deleteCategory(Long categoryId){
-        Category category = categoryRepositroy.findById(categoryId).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND,"Resource not found"));
+        Category category = categoryRepositroy.findById(categoryId).orElseThrow(() -> new ResourceNotFoundException("Category","categoryId",categoryId));
         categoryRepositroy.delete(category);
         return "Category with categoryId: "+categoryId + "deleted successfully";
     }
 
     @Override
     public Category updateCategory(Category category,Long categoryId) {
-         Category savedcategory = categoryRepositroy.findById(categoryId).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+         Category savedcategory = categoryRepositroy.findById(categoryId).orElseThrow(() -> new ResourceNotFoundException("Category","categoryId",categoryId));
          category.setCategoryId(categoryId);
          savedcategory = categoryRepositroy.save(category);
          return savedcategory;
