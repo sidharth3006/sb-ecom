bef1fc6ab11ce69480ffebabd3ea0bdf803f3a2a
Sidharth <sidharth2020sharma@gmail.com>
2026-02-28 22:49:50 +0530

    Add jwt and custom authentication

---
+++ src/main/java/com/ecommerce/project/security/response/UserInfoResponse.java
@@ -0,0 +1,49 @@
+package com.ecommerce.project.security.response;
+
+import java.util.List;
+
+public class UserInfoResponse {
+    private Long id;
+    private String jwtToken;
+    private String username;
+    private List<String> roles;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public UserInfoResponse(Long id, String jwtToken, List<String> roles, String username) {
+        this.id = id;
+        this.jwtToken = jwtToken;
+        this.roles = roles;
+        this.username = username;
+    }
+
+    public String getJwtToken() {
+        return jwtToken;
+    }
+
+    public void setJwtToken(String jwtToken) {
+        this.jwtToken = jwtToken;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public List<String> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(List<String> roles) {
+        this.roles = roles;
+    }
+}
